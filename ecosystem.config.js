f32d2277bd2646cd3848f53d6c3f3e2127b3aee8
mob <tanjiawei@zuoyebang.com>
2022-02-17 14:15:33 +0800

    init project

---
+++ ecosystem.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apps: [
+    {
+      name: "ws-test-server",
+      script: "./src/index.js",
+    },
+  ],
+};
---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(cors());
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.send("<p>ws test server is on.</p>");
+});
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", async (socket) => {
+  console.log("connection on");
+  socket.on("msg", (data) => {
+    socket.emit("msg", data);
+  });
+});
+
+server.listen(4111, () => {
+  console.log("[App] listening on *:3111");
+});
